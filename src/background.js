fabd85128c46574d570bcd19a13ad7ed2d480e37
eulerfan40 <eulerfan40@gmail.com>
2025-03-19 13:02:31 -0700

    Added GUI for moving multiple tabs to new window

--- src/background.js
+++ src/background.js
@@ -1,82 +1,79 @@
-chrome.commands.onCommand.addListener((command) => {
-    switch (command) {
-      case "open-gui":
-        // Open the custom extension webpage
-        chrome.windows.create({
-          url: chrome.runtime.getURL("web/index.html"),
-          type: "popup",
-          width: 800,
-          height: 600
-        });
-        break;
-  
-      case "move-tab-to-new-window":
-        // Move the current tab to a new window
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          const tab = tabs[0];
-          chrome.windows.create({ tabId: tab.id });
-        });
-        break;
-  
-      case "duplicate-tab":
-        // Duplicate the current tab
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          const tab = tabs[0];
-          chrome.tabs.duplicate(tab.id);
-        });
-        break;
-  
-      case "duplicate-tab-to-new-window":
-        // Duplicate the current tab into a new window
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          const tab = tabs[0];
-          chrome.tabs.duplicate(tab.id, (duplicatedTab) => {
-            chrome.windows.create({ tabId: duplicatedTab.id });
-          });
-        });
-        break;
-  
-      default:
-        console.log("Unknown command:", command);
-    }
+function openGUI() {
+  // Open the custom extension webpage in a new window
+  chrome.windows.create({
+    url: chrome.runtime.getURL("web/index.html"),
+    type: "normal",
   });
+}
 
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    switch (message.action) {
-      case "move-tab-to-new-window":
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          const tab = tabs[0];
-          chrome.windows.create({ tabId: tab.id });
-        });
-        break;
-  
-      case "duplicate-tab":
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          const tab = tabs[0];
-          chrome.tabs.duplicate(tab.id);
-        });
-        break;
-  
-      case "duplicate-tab-to-new-window":
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          const tab = tabs[0];
-          chrome.tabs.duplicate(tab.id, (duplicatedTab) => {
-            chrome.windows.create({ tabId: duplicatedTab.id });
-          });
-        });
-        break;
-  
-      case "open-gui":
-        chrome.windows.create({
-          url: chrome.runtime.getURL("web/index.html"),
-          type: "popup",
-          width: 800,
-          height: 600
-        });
-        break;
-  
-      default:
-        console.log("Unknown action:", message.action);
-    }
+function moveTabToNewWindow() {
+  // Move the current tab to a new window
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const tab = tabs[0];
+    chrome.windows.create({ tabId: tab.id });
   });
-  
+}
+
+function duplicateTab() {
+  // Duplicate the current tab
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const tab = tabs[0];
+    chrome.tabs.duplicate(tab.id);
+  });
+}
+
+function duplicateTabToNewWindow() {
+  // Duplicate the current tab into a new window
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const tab = tabs[0];
+    chrome.tabs.duplicate(tab.id, (duplicatedTab) => {
+      chrome.windows.create({ tabId: duplicatedTab.id });
+    });
+  });
+}
+
+chrome.commands.onCommand.addListener((command) => {
+  switch (command) {
+    case "open-gui":
+      openGUI();
+      break;
+
+    case "move-tab-to-new-window":
+      moveTabToNewWindow();
+      break;
+
+    case "duplicate-tab":
+      duplicateTab();
+      break;
+
+    case "duplicate-tab-to-new-window":
+      duplicateTabToNewWindow();
+      break;
+
+    default:
+      console.error('Tabflow: "', message.action, '" command not implemented.');
+  }
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  switch (message.action) {
+    case "open-gui":
+      openGUI();
+      break;
+
+    case "move-tab-to-new-window":
+      moveTabToNewWindow();
+      break;
+
+    case "duplicate-tab":
+      duplicateTab();
+      break;
+
+    case "duplicate-tab-to-new-window":
+      duplicateTabToNewWindow();
+      break;
+
+    default:
+      console.error('Tabflow: "', message.action, '" action not implemented.');
+  }
+});
